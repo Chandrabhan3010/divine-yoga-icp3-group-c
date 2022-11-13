725729d6869a2600fc9a925c683a4c5531f790fb
Chandrabhan Rahangdale <nikhilrahangdale786@gmail.com>
2022-11-13 23:43:23 +0530

    button changed and js file added

---
+++ script/main.js
@@ -0,0 +1,10 @@
+function light(){
+    document.querySelector(".main-container").className = "main-container bg-dark"
+    
+}
+
+function dark(){
+    document.querySelector(".main-container").className = "main-container bg-light"
+   
+
+}
