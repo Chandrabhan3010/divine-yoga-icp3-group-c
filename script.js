c31fccb0173c45ba17bac7f1fb4c732938e0ade9
kanchan <kanchankotkar001@gmail.com>
2022-11-14 14:13:00 +0530

    commit by kanchan

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let myform=document.querySelector('.search-form')
 myuser.onclick =() =>{
     myform.classList.toggle('active')
 }
+
+let mypopup =document.querySelector('#sumbmit')
+let mythanku =document.querySelector('.popup')
+
+mypopup.onclick =() =>{
+    myform.classList.toggle('active')
+}
